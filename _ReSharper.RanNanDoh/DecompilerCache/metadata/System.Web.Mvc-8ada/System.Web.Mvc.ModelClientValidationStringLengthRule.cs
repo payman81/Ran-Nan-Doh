7a44703b3d761308ebbb7c4c225ba70a34d36aea
Payman Labbaf <payman.labbaf@gmail.com>
2012-08-13 21:16:54 +0100

    Resolved conflicting reference

---
+++ _ReSharper.RanNanDoh/DecompilerCache/metadata/System.Web.Mvc-8ada/System.Web.Mvc.ModelClientValidationStringLengthRule.cs
@@ -0,0 +1,15 @@
+// Type: System.Web.Mvc.ModelClientValidationStringLengthRule
+// Assembly: System.Web.Mvc, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35
+// Assembly location: C:\Program Files (x86)\Microsoft ASP.NET\ASP.NET MVC 3\Assemblies\System.Web.Mvc.dll
+
+namespace System.Web.Mvc
+{
+    public class ModelClientValidationStringLengthRule : ModelClientValidationRule
+    {
+        #region Constructors and Destructors
+
+        public ModelClientValidationStringLengthRule(string errorMessage, int minimumLength, int maximumLength);
+
+        #endregion
+    }
+}
